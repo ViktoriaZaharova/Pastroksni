32b1afe3c103b603d68ec4dd7c90084ff3832128
Viktoria <zaharova181114@gmail.com>
2021-08-29 00:31:34 +0300

    add page

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+// активная ссылка меню
+$('.menu li a').each(function () {
+    let location = window.location.href;
+    let link = this.href;
+    if (location === link) {
+        $(this).addClass('active');
+    }
 });
+// end
