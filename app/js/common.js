655bfc0a198be70dc8ce21ee6ddfb92db3ede9de
Viktoria <zaharova181114@gmail.com>
2021-08-29 20:38:03 +0300

    add page2

--- app/js/common.js
+++ app/js/common.js
@@ -7,3 +7,21 @@ $('.menu li a').each(function () {
     }
 });
 // end
+
+//плавный скролл
+$(document).ready(function () {
+    $('.go_to').click(function (e) {
+        e.preventDefault();
+        $('.go_to').removeClass('active');
+        $(this).addClass('active');
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length !== 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_el).offset().top
+            }, 500);
+
+        }
+        return false;
+    });
+});
+//плавный скролл end
